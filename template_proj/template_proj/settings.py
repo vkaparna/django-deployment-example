5e96ff4bbeb1cd08412f35e95f56067e6ca7af0c
Aparna <vkaparnna@gmail.com>
2020-03-17 11:02:01 +0530

    'second'

--- template_proj/template_proj/settings.py
+++ template_proj/template_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cxunlfpcdf=(79$ie#ise96iy=-e$n7@ws#=dj0&_pb#)rhbm%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
